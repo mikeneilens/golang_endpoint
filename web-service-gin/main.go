56a69613421fdaf52cbbd260359d8cf5da891ff3
Mike Neilens <mike.neilens@gmail.com>
2021-11-30 16:35:36 +0000

    sample of the web service from the go tutorial with a few tweaks to try some language features

---
+++ web-service-gin/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+type AlbumsSlice []album
+
+var albums = AlbumsSlice{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func (albumsSlice AlbumsSlice) findAlbum(id string) *album {
+	for _, album := range albumsSlice {
+		if album.ID == id {
+			return &album
+		}
+	}
+	return nil
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumById)
+	router.POST("/albums", postAlbum)
+
+	router.Run("localhost:8080")
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbum(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+
+	if albumPtr := albums.findAlbum(id); albumPtr != nil {
+		c.IndentedJSON(http.StatusOK, *albumPtr)
+	} else {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	}
+}
